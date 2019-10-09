d74ca3fd8ba7536cdca3f74acf5ea38a23805868
font-zyl <18335198709@126.com>
2019-10-09 23:13:14 +0800

    本地数据 调整

--- src/store/module/index.js
+++ src/store/module/index.js
@@ -1,20 +1,61 @@
 import { observable, action, computed } from 'mobx'
-import { queryHistory,queryBooks } from 'src/api/service/Index'
+import { queryHistory,queryBooks,queryReflect,queryActiveBooks,queryBooksList } from 'src/api/service/Index'
 class IndexCommon {
   @observable
-  books = [];
+  IndexBooks = [];
   @observable
   historyList = [];
+  @observable
+  Reflect = [];
+  @observable
+  ActiveBooks = [];
+  @observable
+  BookList = [];
+  @observable
+  BookListPages = null;
+  @observable
+  BookListCount = null;
+
   @action
-  setBooks() {
+  SetBooks() {
     queryBooks().then( res => {
-      this.books = res.data;
+      this.IndexBooks = res.data;
     })
   }
   @action
-  setHistory() {
+  SetBooksList() {
+    queryBooksList().then( res => {
+      this.BookList = res.data.data;
+      this.BookListCount = res.data.count;
+    })
+  }
+  @action
+  addPagesFun(Pages) {
+    this.BookListPages = Pages;
+  }
+  @action
+  addBooksList(Pages) {
+    queryBooksList(Pages).then( res => {
+      this.BookList.concat(res.data.data);
+      this.BookListCount = res.data.count;
+    })
+  }
+  @action
+  SetActiveBooks() {
+    queryActiveBooks().then( res => {
+      this.ActiveBooks = res.data;
+    })
+  }
+  @action
+  SetHistory() {
     queryHistory().then(res => {
       this.historyList = res.data
+    })
+  }
+  @action
+  SetReflect() {
+    queryReflect().then(res => {
+      this.Reflect = res.data
     })
   }
   @computed
@@ -24,7 +65,7 @@ class IndexCommon {
 
   @computed
   get getFilterBooks() {
-    return (this.books.length)?JSON.parse(JSON.stringify(this.books)).slice(0,3):[]
+    return (this.IndexBooks.length)?JSON.parse(JSON.stringify(this.IndexBooks)).slice(0,3):[]
   }
 }
 
